b46a1975da7d0c827dfaad34476c4625be685001
Karla-Isabel-Sandoval <k.isabel.sandoval@gmail.com>
2017-04-10 18:53:49 -0700

    Add a full_title helper

---
+++ constructors-4.4.1-ex.rb
@@ -0,0 +1,3 @@
+puts a = (1..10).to_a
+
+puts a = Array.new(['2,4,5,20'])
---
+++ eachmethod.rb
@@ -0,0 +1,4 @@
+puts (1..5).to_a
+puts (1..5).to_a.inspect
+puts :name, :name.inspect
+puts "It worked", "It worked!".inspect
